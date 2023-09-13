8cd9fd55ba62489185a07c177c4066c5df95f6a6
redwine12 <migonyoung01@gmail.com>
2023-09-13 11:09:25 +0900

    Initial commit

---
+++ iosTimer/iosTimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  iosTimerApp.swift
+//  iosTimer
+//
+//  Created by 이재영 on 2023/09/13.
+//
+
+import SwiftUI
+
+@main
+struct iosTimerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
